eeca1399634981b5f1b6d2f429399e23ae691936
Dominik Meisner <101473446+b14ucky@users.noreply.github.com>
2025-04-10 19:23:25 +0200

    Feat/settings-screen (#2)
    * refactor: customize calendar arrow color and adjust layout styles
    
    * feat: enhance settings screen with user profile and settings items

--- app/(tabs)/(index)/index.tsx
+++ app/(tabs)/(index)/index.tsx
@@ -209,7 +209,6 @@ export default function Index() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: colors.whiteish,
 	},
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,19 +1,94 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { colors } from "@/constants/Colors";
+import Heading from "@/components/ui/Heading";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import Button from "@/components/ui/Button";
+import Constants from "expo-constants";
+import SettingsItem from "@/components/ui/SettingsItem";
+import { shadows } from "@/constants/Shadows";
 
 export default function Settings() {
 	return (
-		<View style={styles.container}>
-			<Text>Settings...</Text>
-		</View>
+		<SafeAreaProvider>
+			<SafeAreaView style={styles.container}>
+				<View style={styles.headingContainer}>
+					<Heading>TWÓJ PROFIL</Heading>
+				</View>
+				<View style={[styles.userContainer, shadows.lightShadow]}>
+					<Ionicons name={"person"} size={50} />
+					<View>
+						<Text style={styles.userName}>Dominik Meisner</Text>
+						<Text style={styles.email}>meisnerd2003@gmail.com</Text>
+					</View>
+				</View>
+				<ScrollView
+					style={styles.settingsContainer}
+					contentContainerStyle={{ alignItems: "center" }}
+				>
+					<SettingsItem
+						text={"Ustawienia profilu"}
+						Icon={Feather}
+						iconName={"user"}
+					/>
+
+					{/* this should always be at the bottom */}
+					<Button
+						text={"Wyloguj się"}
+						Icon={Feather}
+						iconName={"log-out"}
+						buttonProps={{
+							style: { marginTop: 25 },
+						}}
+					/>
+					<Text style={styles.version}>
+						Wersja: {Constants.expoConfig?.version || "błąd"}
+					</Text>
+				</ScrollView>
+			</SafeAreaView>
+		</SafeAreaProvider>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: colors.whiteish,
+	},
+	headingContainer: {
+		padding: 15,
+		marginBottom: 10,
+	},
+	userContainer: {
+		width: "95%",
+		flexDirection: "row",
+		backgroundColor: colors.white,
+		padding: 20,
+		borderRadius: 20,
+		gap: 5,
+		zIndex: 1,
+	},
+	userName: {
+		fontSize: 26,
+		fontWeight: 600,
+		paddingHorizontal: 20,
+		color: colors.black,
+	},
+	email: {
+		fontSize: 14,
+		fontWeight: 300,
+		paddingHorizontal: 20,
+		color: colors.gray,
+	},
+	settingsContainer: {
+		width: "95%",
+		paddingVertical: 20,
+		paddingHorizontal: 15,
+	},
+	version: {
+		marginTop: 10,
+		padding: 5,
+		color: colors.lightGray,
 	},
 });
